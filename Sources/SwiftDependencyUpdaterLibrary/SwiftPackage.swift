0dd5406e9bba5f42f278af0e92a6df41e7e6f94f
Flavien Raynaud <flavien.raynaud@gmail.com>
2022-09-21 15:34:01 +1000

    Improve Package.swift reading
    The current implementation would not be able to read and update
    dependencies with the following Package.swift:
    
    ```swift
    // ...
    dependencies: [
        .package(url: "https://some.url/some/package", exact: .init(1, 0, 0, prereleaseIdentifiers: ["beta"], buildMetadataIdentifiers: [])),
        .package(url: "https://some.other.url/some/otherpackage", .upToNextMajor(from: "1.0.0")),
    ],
    // ...
    ```
    
    This is due to the current regex to match package dependencies, it
    matches the closing bracket from `"beta"` before the one for
    `dependencies`, meaning that `otherpackage` cannot be updated.
    
    This commit relaxes the regex to match only on `.package(...)`, as the
    only position a valid `.package` can be is within the `dependencies`
    block.

--- Sources/SwiftDependencyUpdaterLibrary/SwiftPackage.swift
+++ Sources/SwiftDependencyUpdaterLibrary/SwiftPackage.swift
@@ -29,7 +29,7 @@ struct SwiftPackage {
         // swiftlint:disable:next line_length
         let versionRegExString = "(\\.upToNextMajor\\s*\\(\\s*from\\s*:\\s*\"([0-9]*\\.[0-9]*\\.[0-9]*)\"\\s*\\))|(\\.upToNextMinor\\s*\\(\\s*from\\s*:\\s*\"([0-9]*\\.[0-9]*\\.[0-9]*)\"\\s*\\))|(\\.exact\\s*\\(\\s*\"([0-9]*\\.[0-9]*\\.[0-9]*)\"\\s*\\))|(from\\s*:\\s*\\s*\"([0-9]*\\.[0-9]*\\.[0-9]*)\")|(\\s*\"[0-9]*\\.[0-9]*\\.[0-9]*\"\\.\\.\\.\"([0-9]*\\.[0-9]*\\.[0-9]*)\")|(\\s*\"[0-9]*\\.[0-9]*\\.[0-9]*\"\\.\\.<\"([0-9]*\\.[0-9]*\\.[0-9]*)\")"
         // swiftlint:disable:next line_length
-        let regex = try NSRegularExpression(pattern: "dependencies\\s*:\\s*\\[\\s*[^\\]]*\\.package\\s*\\(\\s*url\\s*:\\s*\"\(NSRegularExpression.escapedPattern(for: dependency.url.absoluteString))\"\\s*,\\s*(\(versionRegExString))", options: [.anchorsMatchLines])
+        let regex = try NSRegularExpression(pattern: "\\.package\\s*\\(\\s*url\\s*:\\s*\"\(NSRegularExpression.escapedPattern(for: dependency.url.absoluteString))\"\\s*,\\s*(\(versionRegExString))", options: [.anchorsMatchLines])
 
         let results = string.matchingStringsWithRange(regex: regex)
         guard results.count == 1, let matches = results[safe: 0] else {
